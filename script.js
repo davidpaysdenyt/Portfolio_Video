ac7916c71b2894a6ff2076c90f528e5c25600ffa
davidpaysdenyt <davidpaysdenyt@gmail.com>
2026-03-23 11:30:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,32 @@ const projects = [
     vimeo: "https://player.vimeo.com/video/987654321",
   }
 ];
+
+const grid = document.querySelector(".grid");
+
+projects.forEach(project => {
+  const item = document.createElement("div");
+  item.classList.add("item");
+  item.setAttribute("data-video", project.vimeo);
+
+  const video = document.createElement("video");
+  video.src = project.preview;
+  video.muted = true;
+  video.loop = true;
+
+  item.appendChild(video);
+  grid.appendChild(item);
+
+  // hover
+  item.addEventListener("mouseenter", () => video.play());
+  item.addEventListener("mouseleave", () => video.pause());
+
+  // click
+  item.addEventListener("click", () => {
+    player.src = project.vimeo + "?autoplay=1";
+    modal.style.display = "flex";
+  });
+});
     
     // click = open Vimeo
     item.addEventListener("click", () => {
