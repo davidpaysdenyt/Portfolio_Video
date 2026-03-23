0eb0234ff0246c31052e786fccd621e3c19b0174
davidpaysdenyt <davidpaysdenyt@gmail.com>
2026-03-23 16:01:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,66 +16,79 @@ const projects = [
   {
     preview: "videos/AFTERTYLERPREV.mp4",
     vimeo: "https://player.vimeo.com/video/987654321",
+    tags: ["Music-video"],
     order: 6
   },
   {
     preview: "videos/NEWSEN.mp4",
     vimeo: "https://player.vimeo.com/video/987654321",
+    tags: ["Music-video"],
     order: 3
   },
   {
     preview: "videos/VOLMAG.mp4",
     vimeo: "https://player.vimeo.com/video/987654321",
+    tags: ["Sport"],
     order: 4
   },
   {
     preview: "videos/OLSEN.mp4",
     vimeo: "https://player.vimeo.com/video/987654321",
+    tags: ["Sport"],
     order: 5
   },
   {
     preview: "videos/CORPAUT.mp4",
     vimeo: "https://player.vimeo.com/video/987654321",
+    tags: ["Corporate"],
     order: 2
   },
   {
     preview: "videos/EPSTADIO.mp4",
     vimeo: "https://player.vimeo.com/video/987654321",
+    tags: ["Documentary"],
     order: 7
   },
   {
     preview: "videos/QSA.mp4",
     vimeo: "https://player.vimeo.com/video/987654321",
+    tags: ["Sport"],
     order: 8
   },
   {
     preview: "videos/VILLAIGEA.mp4",
     vimeo: "https://player.vimeo.com/video/987654321",
+    tags: ["Documentary"],
     order: 9
   },
   {
     preview: "videos/INTERLUDE.mp4",
     vimeo: "https://player.vimeo.com/video/987654321",
+    tags: ["Music-video"],
     order: 10
   },
   {
     preview: "videos/FVBARCA.mp4",
     vimeo: "https://player.vimeo.com/video/987654321",
+    tags: ["Luxury"],
     order: 11
   },
   {
     preview: "videos/SERAFINO.mp4",
     vimeo: "https://player.vimeo.com/video/987654321",
+    tags: ["Luxury"],
     order: 12
   },
   {
     preview: "videos/THALASSO.mp4",
     vimeo: "https://player.vimeo.com/video/987654321",
+    tags: ["Luxury"],
     order: 13
   },
   {
     preview: "videos/PARMA.mp4",
     vimeo: "https://player.vimeo.com/video/987654321",
+    tags: ["Sport"],
     order: 14
   }
 ];
