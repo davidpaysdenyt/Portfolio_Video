96809345ae4f3049a76e444e9a878985b22b1ee2
davidpaysdenyt <davidpaysdenyt@gmail.com>
2026-03-26 15:02:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let items = [];
 const projects = [
   {
     preview: "videos/WORKPREV.mp4",
-    vimeo: "https://www.youtube.com/embed/S3b_-2h4nk4?si=z9Kic34MZ2t2k09G",
+    vimeo: "https://player.vimeo.com/video/1177328732",
     tags: ["Music-video"],
     order: 1
   },
@@ -22,43 +22,43 @@ const projects = [
   },
   {
     preview: "videos/NEWSEN.mp4",
-    vimeo: "https://www.youtube.com/embed/IsUeL8t_RK0?si=yui-RqgPSjVFJ5Rt",
+    vimeo: "https://player.vimeo.com/video/1177329107",
     tags: ["Music-video"],
     order: 3
   },
   {
     preview: "videos/VOLMAG.mp4",
-    vimeo: "https://www.youtube.com/embed/b2mbeBbVEmk?si=4tYZd47A76Rp1jYS",
+    vimeo: "https://player.vimeo.com/video/1177277852",
     tags: ["Sport"],
     order: 4
   },
   {
     preview: "videos/OLSEN.mp4",
-    vimeo: "https://www.youtube.com/embed/cFAtHOUG0tc?si=s1RhJQ5-N7YaAwpY",
+    vimeo: "https://player.vimeo.com/video/1177269065",
     tags: ["Sport"],
     order: 5
   },
   {
     preview: "videos/CORPAUT.mp4",
-    vimeo: "https://player.vimeo.com/video/987654321",
+    vimeo: "https://player.vimeo.com/video/1177268281",
     tags: ["Corporate"],
     order: 2
   },
   {
     preview: "videos/EPSTADIO.mp4",
-    vimeo: "https://www.youtube.com/embed/65rFOtDd_58?si=73oNV7ACBPrIYufY",
+    vimeo: "https://player.vimeo.com/video/1177269371",
     tags: ["Documentary"],
     order: 7
   },
   {
     preview: "videos/QSA.mp4",
-    vimeo: "https://www.youtube.com/embed/B_AnCQrNWEU?si=VK0exjLnVF18engs",
+    vimeo: "https://player.vimeo.com/video/1177268731",
     tags: ["Sport"],
     order: 8
   },
   {
     preview: "videos/VILLAIGEA.mp4",
-    vimeo: "https://www.youtube.com/embed/05XQRsQS7V0?si=jMtQM1OUxP6kn8-8",
+    vimeo: "https://player.vimeo.com/video/1177275953",
     tags: ["Documentary"],
     order: 9
   },
@@ -70,91 +70,97 @@ const projects = [
   },
   {
     preview: "videos/FVBARCA.mp4",
-    vimeo: "https://player.vimeo.com/video/987654321",
+    vimeo: "https://player.vimeo.com/video/1177269148",
     tags: ["Luxury"],
     order: 11
   },
   {
     preview: "videos/SERAFINO.mp4",
-    vimeo: "https://player.vimeo.com/video/987654321",
+    vimeo: "https://player.vimeo.com/video/1177331592",
     tags: ["Luxury"],
     order: 12
   },
   {
     preview: "videos/THALASSO.mp4",
-    vimeo: "https://player.vimeo.com/video/987654321",
+    vimeo: "https://player.vimeo.com/video/1177269209",
     tags: ["Luxury"],
     order: 13
   },
   {
     preview: "videos/PARMA.mp4",
-    vimeo: "https://player.vimeo.com/video/987654321",
+    vimeo: "https://player.vimeo.com/video/1177277040",
     tags: ["Sport"],
     order: 14
   },
   {
     preview: "videos/aea.mp4",
-    vimeo: "https://player.vimeo.com/video/987654321",
+    vimeo: "https://player.vimeo.com/video/1177278777",
     tags: ["Corporate"],
     order: 15
   },
   {
     preview: "videos/moretti.mp4",
-    vimeo: "https://player.vimeo.com/video/987654321",
+    vimeo: "https://player.vimeo.com/video/1177277237",
     tags: ["Luxury"],
     order: 16
   },
   {
     preview: "videos/des.mp4",
-    vimeo: "https://player.vimeo.com/video/987654321",
+    vimeo: "https://player.vimeo.com/video/1177279400",
     tags: ["motion"],
     order: 17
   },
   {
     preview: "videos/bike.mp4",
-    vimeo: "https://player.vimeo.com/video/987654321",
+    vimeo: "https://player.vimeo.com/video/1177278709",
     tags: ["motion"],
     order: 18
   },
   {
     preview: "videos/atb.mp4",
-    vimeo: "https://player.vimeo.com/video/987654321",
+    vimeo: "https://player.vimeo.com/video/1177269032",
     tags: ["motion"],
     order: 19
   },
   {
     preview: "videos/chloe.mp4",
-    vimeo: "https://player.vimeo.com/video/987654321",
+    vimeo: "https://player.vimeo.com/video/1177279372",
     tags: ["motion"],
     order: 20
   },
   {
     preview: "videos/golden.mp4",
-    vimeo: "https://player.vimeo.com/video/987654321",
+    vimeo: "https://player.vimeo.com/video/1177279461",
     tags: ["motion"],
     order: 21
   },
   {
     preview: "videos/road.mp4",
-    vimeo: "https://player.vimeo.com/video/987654321",
+    vimeo: "https://player.vimeo.com/video/1177279670",
     tags: ["motion"],
     order: 22
   },
   {
     preview: "videos/estetica.mp4",
-    vimeo: "https://player.vimeo.com/video/987654321",
+    vimeo: "https://player.vimeo.com/video/1177279434",
     tags: ["motion"],
     order: 23
   },
   {
     preview: "videos/hydro.mp4",
-    vimeo: "https://player.vimeo.com/video/987654321",
+    vimeo: "https://player.vimeo.com/video/1177279697",
     tags: ["motion"],
     order: 24
   },
   {
     preview: "videos/avant.mp4",
-    vimeo: "https://player.vimeo.com/video/987654321",
+    vimeo: "https://player.vimeo.com/video/1177279484",
+    tags: ["motion"],
+    order: 25
+  },
+  {
+    preview: "videos/avant.mp4",
+    vimeo: "https://player.vimeo.com/video/1177279484",
     tags: ["motion"],
     order: 25
   }
